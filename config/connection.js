bf086b23db95f566b7fc289ade308eeae849bcb6
Yangyin Hu <yangyinhu@MacBook-Pro.local>
2019-05-26 23:36:18 -0400

    added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "YHuPass123$",
-  database: "burgers_db"
-});
+if (process.env.JAWSBD_URL) {
+  connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "YHuPass123$",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
